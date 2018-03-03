73b052e7040d271147f014ab1460a5315e414a9d
Malgorzata Kudlak <m.kudlak@icloud.com>
2018-03-03 17:11:04 +0100

    aplication working - not finished, add files for exe and scripts, add script Title with style

---
+++ zad2/src/components/Title.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import uuid from 'uuid';
+import style from './Title.css';
+
+const Tytul = props => {
+    return (
+        <div className={style.Title}>
+            <h1>Hello i have a created a magic app</h1>
+        </div>
+
+    )
+}
+
+export default Title;
---
+++ zad2/src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './containers/App';
+import uuid from 'uuid';
+//import Title from './components/Title';
+
+ReactDOM.render(
+    <App />,
+    document.getElementById('app')
+);
