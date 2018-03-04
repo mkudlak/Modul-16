dbf2e3aa38e44a2a0fcd3da07795931a752c0025
Malgorzata Kudlak <m.kudlak@icloud.com>
2018-03-04 17:37:33 +0100

    Add plugins for exe

---
+++ zad3/src/components/Title.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import uuid from 'uuid';
+import style from './Title.css';
+
+const Title = props => <h1>{props.title}</h1>
+
+export default Title;
---
+++ zad3/src/components/TodoList.js
@@ -0,0 +1,6 @@
+h1 {
+    background-color: rgb(84, 169, 179);
+    color: white;
+    font-size: 50px;
+    text-align: center;
+}
